c6ec0f702d6c65611e84bdc210d03b9cc282b5d1
sujintc <133184229+sujintc@users.noreply.github.com>
2024-06-21 10:31:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://chatfront-omega.vercel.app',
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Content-Type', 'Authorization'],
